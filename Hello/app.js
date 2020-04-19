16d1e7366ddc1f0a5d98a7dea022353d02b8a75b
Domingo Perez <djperezh@users.noreply.github.com>
2020-04-19 01:36:43 -0700

    use ES6 sintax

--- Hello/app.js
+++ Hello/app.js
@@ -2,18 +2,24 @@ var msg = 'hello world'
 console.log(msg)
 
 // Demo: Use Module
-var myModule = require('./myModule.js')
-console.log(myModule.myText)
+
+// Old style
+// var myModule = require('./myModule.js')
+// console.log(myText)
+
+// ES6 style
+import { myText } from './myModule.js'
+console.log(myText)
 
 // Demo: Use lodash
-var _ = require('lodash')
-console.log(_.random(1, 40))
+import { random } from 'lodash'
+console.log(random(1, 40))
 
 // Demo: reading from file
 //var data = require('./data.json')
 //console.log(data.name)
-var fs = require('fs')
-fs.readFile('./test.json', 'utf-8', (err,data) => {
+import { readFile } from 'fs'
+readFile('./test.json', 'utf-8', (err,data) => {
     var data = JSON.parse(data)
     console.log(data.name)
 })
